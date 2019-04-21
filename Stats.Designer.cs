5ffbe77c39eeaf12cd673a9f3763c4e5ab9c997c
SmashScharrer <nathan.guirado-patrico@outlook.fr>
2019-04-21 02:12:41 +0200

    Added Design for Statistique (Label ...) + Added Method for Statistiques Data + Added Event to Change Data in term of ID + Negated redirection links

--- Stats.Designer.cs
+++ Stats.Designer.cs
@@ -42,33 +42,33 @@
             this.l_titre_stats = new System.Windows.Forms.Label();
             this.panel_titre = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.panel_date = new System.Windows.Forms.Panel();
-            this.l_titre_date = new System.Windows.Forms.Label();
-            this.panel_puisAbs = new System.Windows.Forms.Panel();
-            this.l_titre_puisAbs = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.l_Duree = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.l_PuisAbs = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.l_Date = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.l_Titre = new System.Windows.Forms.Label();
             this.panel_duree = new System.Windows.Forms.Panel();
             this.l_titre_duree = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.l_titre = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.l_date = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.l_puisAbs = new System.Windows.Forms.Label();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.l_duree = new System.Windows.Forms.Label();
+            this.panel_puisAbs = new System.Windows.Forms.Panel();
+            this.l_titre_puisAbs = new System.Windows.Forms.Label();
+            this.panel_date = new System.Windows.Forms.Panel();
+            this.l_titre_date = new System.Windows.Forms.Label();
             this.p_titre.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ch_Graphe)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.panel_titre.SuspendLayout();
             this.panel4.SuspendLayout();
-            this.panel_date.SuspendLayout();
-            this.panel_puisAbs.SuspendLayout();
-            this.panel_duree.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
             this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel_duree.SuspendLayout();
+            this.panel_puisAbs.SuspendLayout();
+            this.panel_date.SuspendLayout();
             this.SuspendLayout();
             // 
             // p_titre
@@ -111,6 +111,7 @@
             this.cb_Bornes.FormattingEnabled = true;
             resources.ApplyResources(this.cb_Bornes, "cb_Bornes");
             this.cb_Bornes.Name = "cb_Bornes";
+            this.cb_Bornes.SelectedIndexChanged += new System.EventHandler(this.ComboIndexChange);
             // 
             // lb_TitreCb
             // 
@@ -154,31 +155,57 @@
             resources.ApplyResources(this.panel4, "panel4");
             this.panel4.Name = "panel4";
             // 
-            // panel_date
+            // panel5
             // 
-            this.panel_date.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
-            this.panel_date.Controls.Add(this.l_titre_date);
-            resources.ApplyResources(this.panel_date, "panel_date");
-            this.panel_date.Name = "panel_date";
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
+            this.panel5.Controls.Add(this.l_Duree);
+            resources.ApplyResources(this.panel5, "panel5");
+            this.panel5.Name = "panel5";
             // 
-            // l_titre_date
+            // l_Duree
             // 
-            resources.ApplyResources(this.l_titre_date, "l_titre_date");
-            this.l_titre_date.ForeColor = System.Drawing.Color.White;
-            this.l_titre_date.Name = "l_titre_date";
+            resources.ApplyResources(this.l_Duree, "l_Duree");
+            this.l_Duree.ForeColor = System.Drawing.Color.White;
+            this.l_Duree.Name = "l_Duree";
             // 
-            // panel_puisAbs
+            // panel3
             // 
-            this.panel_puisAbs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
-            this.panel_puisAbs.Controls.Add(this.l_titre_puisAbs);
-            resources.ApplyResources(this.panel_puisAbs, "panel_puisAbs");
-            this.panel_puisAbs.Name = "panel_puisAbs";
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
+            this.panel3.Controls.Add(this.l_PuisAbs);
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Name = "panel3";
             // 
-            // l_titre_puisAbs
+            // l_PuisAbs
             // 
-            resources.ApplyResources(this.l_titre_puisAbs, "l_titre_puisAbs");
-            this.l_titre_puisAbs.ForeColor = System.Drawing.Color.White;
-            this.l_titre_puisAbs.Name = "l_titre_puisAbs";
+            resources.ApplyResources(this.l_PuisAbs, "l_PuisAbs");
+            this.l_PuisAbs.ForeColor = System.Drawing.Color.White;
+            this.l_PuisAbs.Name = "l_PuisAbs";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
+            this.panel2.Controls.Add(this.l_Date);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // l_Date
+            // 
+            resources.ApplyResources(this.l_Date, "l_Date");
+            this.l_Date.ForeColor = System.Drawing.Color.White;
+            this.l_Date.Name = "l_Date";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
+            this.panel1.Controls.Add(this.l_Titre);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // l_Titre
+            // 
+            resources.ApplyResources(this.l_Titre, "l_Titre");
+            this.l_Titre.ForeColor = System.Drawing.Color.White;
+            this.l_Titre.Name = "l_Titre";
             // 
             // panel_duree
             // 
@@ -193,57 +220,31 @@
             this.l_titre_duree.ForeColor = System.Drawing.Color.White;
             this.l_titre_duree.Name = "l_titre_duree";
             // 
-            // panel1
+            // panel_puisAbs
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
-            this.panel1.Controls.Add(this.l_titre);
-            resources.ApplyResources(this.panel1, "panel1");
-            this.panel1.Name = "panel1";
+            this.panel_puisAbs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
+            this.panel_puisAbs.Controls.Add(this.l_titre_puisAbs);
+            resources.ApplyResources(this.panel_puisAbs, "panel_puisAbs");
+            this.panel_puisAbs.Name = "panel_puisAbs";
             // 
-            // l_titre
+            // l_titre_puisAbs
             // 
-            resources.ApplyResources(this.l_titre, "l_titre");
-            this.l_titre.ForeColor = System.Drawing.Color.White;
-            this.l_titre.Name = "l_titre";
+            resources.ApplyResources(this.l_titre_puisAbs, "l_titre_puisAbs");
+            this.l_titre_puisAbs.ForeColor = System.Drawing.Color.White;
+            this.l_titre_puisAbs.Name = "l_titre_puisAbs";
             // 
-            // panel2
+            // panel_date
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
-            this.panel2.Controls.Add(this.l_date);
-            resources.ApplyResources(this.panel2, "panel2");
-            this.panel2.Name = "panel2";
+            this.panel_date.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
+            this.panel_date.Controls.Add(this.l_titre_date);
+            resources.ApplyResources(this.panel_date, "panel_date");
+            this.panel_date.Name = "panel_date";
             // 
-            // l_date
+            // l_titre_date
             // 
-            resources.ApplyResources(this.l_date, "l_date");
-            this.l_date.ForeColor = System.Drawing.Color.White;
-            this.l_date.Name = "l_date";
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
-            this.panel3.Controls.Add(this.l_puisAbs);
-            resources.ApplyResources(this.panel3, "panel3");
-            this.panel3.Name = "panel3";
-            // 
-            // l_puisAbs
-            // 
-            resources.ApplyResources(this.l_puisAbs, "l_puisAbs");
-            this.l_puisAbs.ForeColor = System.Drawing.Color.White;
-            this.l_puisAbs.Name = "l_puisAbs";
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
-            this.panel5.Controls.Add(this.l_duree);
-            resources.ApplyResources(this.panel5, "panel5");
-            this.panel5.Name = "panel5";
-            // 
-            // l_duree
-            // 
-            resources.ApplyResources(this.l_duree, "l_duree");
-            this.l_duree.ForeColor = System.Drawing.Color.White;
-            this.l_duree.Name = "l_duree";
+            resources.ApplyResources(this.l_titre_date, "l_titre_date");
+            this.l_titre_date.ForeColor = System.Drawing.Color.White;
+            this.l_titre_date.Name = "l_titre_date";
             // 
             // Stats
             // 
@@ -267,20 +268,20 @@
             this.panel_titre.ResumeLayout(false);
             this.panel_titre.PerformLayout();
             this.panel4.ResumeLayout(false);
-            this.panel_date.ResumeLayout(false);
-            this.panel_date.PerformLayout();
-            this.panel_puisAbs.ResumeLayout(false);
-            this.panel_puisAbs.PerformLayout();
-            this.panel_duree.ResumeLayout(false);
-            this.panel_duree.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel_duree.ResumeLayout(false);
+            this.panel_duree.PerformLayout();
+            this.panel_puisAbs.ResumeLayout(false);
+            this.panel_puisAbs.PerformLayout();
+            this.panel_date.ResumeLayout(false);
+            this.panel_date.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -305,12 +306,12 @@
         private System.Windows.Forms.Panel panel_date;
         private System.Windows.Forms.Label l_titre_date;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label l_titre;
+        private System.Windows.Forms.Label l_Titre;
         private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Label l_duree;
+        private System.Windows.Forms.Label l_Duree;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Label l_puisAbs;
+        private System.Windows.Forms.Label l_PuisAbs;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label l_date;
+        private System.Windows.Forms.Label l_Date;
     }
 }
--- Stats.cs
+++ Stats.cs
@@ -12,7 +12,8 @@ namespace ProjetChargeon
 {
     public partial class Stats : Form
     {
-        CrudBornes selectBornes = new CrudBornes(); // Connexion à la BDD
+        CrudBornes selectBornes = new CrudBornes(); // Connexion à la BDD pour la liste des bornes
+
 
         public Stats()
         {
@@ -35,5 +36,56 @@ namespace ProjetChargeon
 			Logged_Admin.ShowDialog();
 			Close();
 		}
-	}
+
+        private void ComboIndexChange(object sender, EventArgs e)
+        {
+            string idSelected = cb_Bornes.SelectedValue.ToString(); // idSelected vaut l'ID du champ de la ComboBox
+
+            CrudBornes selectDetailsStats = new CrudBornes();
+            DataSet listeDetailsStats = selectDetailsStats.SelectDetailsStats(idSelected);
+
+            // Affichage des données dans chaque label
+
+            // Condition pour Vérifier l'éxistence de la donnée Titre
+            if (listeDetailsStats.Tables[0].Rows[0].ItemArray[0].ToString() != null)
+            {
+                l_Titre.Text = listeDetailsStats.Tables[0].Rows[0].ItemArray[0].ToString(); // Affiche le titre de la Stat
+            }
+            else
+            {
+                l_Titre.Text = "Vide";
+            }
+
+            // Condition pour Vérifier l'éxistence de la donnée Date
+            if (listeDetailsStats.Tables[0].Rows[0].ItemArray[1].ToString() != null)
+            {
+                l_Date.Text = listeDetailsStats.Tables[0].Rows[0].ItemArray[1].ToString(); // Affiche la date de la Stat
+            }
+            else
+            {
+                l_Date.Text = "Vide";
+            }
+
+            // Condition pour Vérifier l'éxistence de la donnée Puissance Absorbée
+            if (listeDetailsStats.Tables[0].Rows[0].ItemArray[2].ToString() != null)
+            {
+                l_PuisAbs.Text = listeDetailsStats.Tables[0].Rows[0].ItemArray[2].ToString(); // Affiche la Puissance Absorbée de la borne
+            }
+            else
+            {
+                l_PuisAbs.Text = "Vide";
+            }
+
+            // Condition pour Vérifier l'éxistence de la donnée Durée
+            if (listeDetailsStats.Tables[0].Rows[0].ItemArray[3].ToString() != null)
+            {
+                l_Duree.Text = listeDetailsStats.Tables[0].Rows[0].ItemArray[3].ToString(); // Affiche la Durée de Chargement de la borne
+            }
+            else
+            {
+                l_Duree.Text = "Vide";
+            }
+
+        }
+    }
 }
