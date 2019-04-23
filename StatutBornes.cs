811ddf8c2cc8dcd8b330073c0c925fc361d45b0e
Kayoshi-dev <maxime.jme@gmail.com>
2019-04-23 02:02:14 +0200

    change database, fix statutBornes consequently

--- StatutBornes.cs
+++ StatutBornes.cs
@@ -44,18 +44,27 @@ namespace ProjetChargeon
 
 			l_id.Text = listeDetailsBornes.Tables[0].Rows[0].ItemArray[0].ToString(); // Affiche l'ID
 			l_zone.Text = listeDetailsBornes.Tables[0].Rows[0].ItemArray[1].ToString(); // Affiche la zone
-			l_desc.Text = listeDetailsBornes.Tables[0].Rows[0].ItemArray[7].ToString(); // Affiche la description
-			l_ns.Text = listeDetailsBornes.Tables[0].Rows[0].ItemArray[8].ToString(); // Affiche le Numéro de série
-			l_type.Text = listeDetailsBornes.Tables[0].Rows[0].ItemArray[9].ToString(); // Affiche le type de borne
-			if (listeDetailsBornes.Tables[0].Rows[0].ItemArray[10].ToString() == "True") // Cette condition change la couleur du texte selon l'état de la borne
+			l_desc.Text = listeDetailsBornes.Tables[0].Rows[0].ItemArray[6].ToString(); // Affiche la description
+			l_ns.Text = listeDetailsBornes.Tables[0].Rows[0].ItemArray[7].ToString(); // Affiche le Numéro de série
+			if(listeDetailsBornes.Tables[0].Rows[0].ItemArray[8].ToString() == "False") 
+			{
+				l_type.Text = "Intérieur";
+			}
+			else 
+			{
+				l_type.Text = "Extérieur";
+			}
+			if (listeDetailsBornes.Tables[0].Rows[0].ItemArray[9].ToString() == "True") // Cette condition change la couleur du texte selon l'état de la borne
 			{
 				l_etat.Text = "ON";
 				l_etat.ForeColor = Color.FromArgb(46, 204, 113); // Vert
-			} else {
+			} 
+			else 
+			{
 				l_etat.Text = "OFF";
 				l_etat.ForeColor = Color.FromArgb(231, 76, 60); // Rouge
 			}
-			l_power.Text = listeDetailsBornes.Tables[0].Rows[0].ItemArray[11].ToString() + " kWh";
+			l_power.Text = listeDetailsBornes.Tables[0].Rows[0].ItemArray[10].ToString() + " kWh";
 		}
 
 		private void BackForm(object sender, EventArgs e)
