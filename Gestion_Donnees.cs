b5ffff8f9935375724b2e8f71f38b48c74588e91
Maxime <maxime.jme@gmail.com>
2019-04-30 11:14:24 +0200

    Revert "Pull request to merge technicien to master"

--- Gestion_Donnees.cs
+++ Gestion_Donnees.cs
@@ -53,23 +53,5 @@ namespace ProjetChargeon
             Bornes.ShowDialog();
             Close();
         }
-
-        // Au Clic, redirection vers Gestion des Techniciens
-        private void onClickToTechnicien(object sender, EventArgs e)
-        {
-            Hide();
-            var Technicien = new Technicien();
-            Technicien.ShowDialog();
-            Close();
-        }
-
-        // Au Clic, redirection vers le CRUD Technicien
-        private void onClickToCRUDTechnicien(object sender, EventArgs e)
-        {
-            Hide();
-            var CRUD_Technicien = new TechnicienCRUD();
-            CRUD_Technicien.ShowDialog();
-            Close();
-        }
     }
 }
