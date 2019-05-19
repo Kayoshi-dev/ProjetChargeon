1f7f2bda5eb660dc0b8f038fa817fd06db874903
Kayoshi-dev <maxime.jme@gmail.com>
2019-05-19 02:28:06 +0200

    Add iTextSharp, and a method to create a PDF

--- Facturation.Designer.cs
+++ Facturation.Designer.cs
@@ -28,155 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Facturation));
-			this.p_titre = new System.Windows.Forms.Panel();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			this.Id_facture = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.titre_facture = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.montant_facture = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.dateDeb_facture = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.dateFin_facture = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.type_fac = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.pictureBox2 = new System.Windows.Forms.PictureBox();
-			this.p_titre.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// p_titre
-			// 
-			this.p_titre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(70)))), ((int)(((byte)(99)))));
-			this.p_titre.Controls.Add(this.pictureBox2);
-			this.p_titre.Controls.Add(this.pictureBox1);
-			this.p_titre.Controls.Add(this.label1);
-			this.p_titre.Dock = System.Windows.Forms.DockStyle.Top;
-			this.p_titre.Location = new System.Drawing.Point(0, 0);
-			this.p_titre.Name = "p_titre";
-			this.p_titre.Size = new System.Drawing.Size(800, 73);
-			this.p_titre.TabIndex = 4;
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-			this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-			this.pictureBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.pictureBox1.Location = new System.Drawing.Point(758, 12);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(30, 30);
-			this.pictureBox1.TabIndex = 2;
-			this.pictureBox1.TabStop = false;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.ForeColor = System.Drawing.SystemColors.Window;
-			this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.label1.Location = new System.Drawing.Point(12, 25);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(90, 21);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Chargéon";
-			// 
-			// dataGridView1
-			// 
-			this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Id_facture,
-            this.titre_facture,
-            this.montant_facture,
-            this.dateDeb_facture,
-            this.dateFin_facture,
-            this.type_fac});
-			this.dataGridView1.Location = new System.Drawing.Point(12, 79);
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(776, 359);
-			this.dataGridView1.TabIndex = 5;
-			// 
-			// Id_facture
-			// 
-			this.Id_facture.HeaderText = "Id ";
-			this.Id_facture.Name = "Id_facture";
-			this.Id_facture.Width = 80;
-			// 
-			// titre_facture
-			// 
-			this.titre_facture.HeaderText = "Facture";
-			this.titre_facture.Name = "titre_facture";
-			this.titre_facture.Width = 150;
-			// 
-			// montant_facture
-			// 
-			this.montant_facture.HeaderText = "Montant";
-			this.montant_facture.Name = "montant_facture";
-			this.montant_facture.Width = 150;
-			// 
-			// dateDeb_facture
-			// 
-			this.dateDeb_facture.HeaderText = "Date début";
-			this.dateDeb_facture.Name = "dateDeb_facture";
-			// 
-			// dateFin_facture
-			// 
-			this.dateFin_facture.HeaderText = "Date fin";
-			this.dateFin_facture.Name = "dateFin_facture";
-			// 
-			// type_fac
-			// 
-			this.type_fac.HeaderText = "Type";
-			this.type_fac.Name = "type_fac";
-			this.type_fac.Width = 150;
-			// 
-			// pictureBox2
-			// 
-			this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(70)))), ((int)(((byte)(99)))));
-			this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-			this.pictureBox2.Cursor = System.Windows.Forms.Cursors.PanWest;
-			this.pictureBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.pictureBox2.Location = new System.Drawing.Point(707, 12);
-			this.pictureBox2.Name = "pictureBox2";
-			this.pictureBox2.Size = new System.Drawing.Size(30, 30);
-			this.pictureBox2.TabIndex = 16;
-			this.pictureBox2.TabStop = false;
-			this.pictureBox2.Click += new System.EventHandler(this.BackForm);
-			// 
-			// Facturation
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.dataGridView1);
-			this.Controls.Add(this.p_titre);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Name = "Facturation";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "facturation";
-			this.p_titre.ResumeLayout(false);
-			this.p_titre.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Facturation));
+            this.l_Titre_Chargeon = new System.Windows.Forms.Label();
+            this.p_form1 = new System.Windows.Forms.Panel();
+            this.img_Return = new System.Windows.Forms.PictureBox();
+            this.img_Close = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rtb_data = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.p_form1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img_Return)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_Close)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // l_Titre_Chargeon
+            // 
+            this.l_Titre_Chargeon.AutoSize = true;
+            this.l_Titre_Chargeon.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.l_Titre_Chargeon.ForeColor = System.Drawing.SystemColors.Window;
+            this.l_Titre_Chargeon.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.l_Titre_Chargeon.Location = new System.Drawing.Point(12, 25);
+            this.l_Titre_Chargeon.Name = "l_Titre_Chargeon";
+            this.l_Titre_Chargeon.Size = new System.Drawing.Size(90, 21);
+            this.l_Titre_Chargeon.TabIndex = 4;
+            this.l_Titre_Chargeon.Text = "Chargéon";
+            // 
+            // p_form1
+            // 
+            this.p_form1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(70)))), ((int)(((byte)(99)))));
+            this.p_form1.Controls.Add(this.img_Return);
+            this.p_form1.Controls.Add(this.l_Titre_Chargeon);
+            this.p_form1.Controls.Add(this.img_Close);
+            this.p_form1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.p_form1.Location = new System.Drawing.Point(0, 0);
+            this.p_form1.Name = "p_form1";
+            this.p_form1.Size = new System.Drawing.Size(800, 72);
+            this.p_form1.TabIndex = 13;
+            // 
+            // img_Return
+            // 
+            this.img_Return.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(70)))), ((int)(((byte)(99)))));
+            this.img_Return.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("img_Return.BackgroundImage")));
+            this.img_Return.Cursor = System.Windows.Forms.Cursors.PanWest;
+            this.img_Return.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.img_Return.Location = new System.Drawing.Point(707, 12);
+            this.img_Return.Name = "img_Return";
+            this.img_Return.Size = new System.Drawing.Size(30, 30);
+            this.img_Return.TabIndex = 13;
+            this.img_Return.TabStop = false;
+            // 
+            // img_Close
+            // 
+            this.img_Close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.img_Close.Image = ((System.Drawing.Image)(resources.GetObject("img_Close.Image")));
+            this.img_Close.Location = new System.Drawing.Point(758, 12);
+            this.img_Close.Name = "img_Close";
+            this.img_Close.Size = new System.Drawing.Size(30, 30);
+            this.img_Close.TabIndex = 1;
+            this.img_Close.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 21);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Mes factures";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(16, 108);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(166, 29);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(337, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 21);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Date début";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(627, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 21);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Date fin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(337, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 21);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(627, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 21);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "label5";
+            // 
+            // rtb_data
+            // 
+            this.rtb_data.Cursor = System.Windows.Forms.Cursors.No;
+            this.rtb_data.Location = new System.Drawing.Point(16, 158);
+            this.rtb_data.Name = "rtb_data";
+            this.rtb_data.Size = new System.Drawing.Size(772, 238);
+            this.rtb_data.TabIndex = 20;
+            this.rtb_data.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(657, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 31);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Telecharger";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Facturation
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(61)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtb_data);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.p_form1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Facturation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Facturation";
+            this.p_form1.ResumeLayout(false);
+            this.p_form1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img_Return)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_Close)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel p_titre;
+        private System.Windows.Forms.Label l_Titre_Chargeon;
+        private System.Windows.Forms.Panel p_form1;
+        private System.Windows.Forms.PictureBox img_Return;
+        private System.Windows.Forms.PictureBox img_Close;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Id_facture;
-        private System.Windows.Forms.DataGridViewTextBoxColumn titre_facture;
-        private System.Windows.Forms.DataGridViewTextBoxColumn montant_facture;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dateDeb_facture;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dateFin_facture;
-        private System.Windows.Forms.DataGridViewTextBoxColumn type_fac;
-        private System.Windows.Forms.PictureBox pictureBox1;
-		private System.Windows.Forms.PictureBox pictureBox2;
-	}
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox rtb_data;
+        private System.Windows.Forms.Button button1;
+    }
 }
--- Facturation.cs
+++ Facturation.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +20,29 @@ namespace ProjetChargeon
             InitializeComponent();
         }
 
-		private void BackForm(object sender, EventArgs e)
-		{
-			Hide();
-			var Logged_Customer = new Logged_Customer();
-			Logged_Customer.ShowDialog();
-			Close();
-		}
-	}
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using(SaveFileDialog sfd = new SaveFileDialog() { Filter = "PDF file|*.pdf", ValidateNames = true })
+            {
+                if(sfd.ShowDialog() == DialogResult.OK)
+                {
+                    iTextSharp.text.Document doc = new iTextSharp.text.Document(PageSize.A4.Rotate());
+                    try
+                    {
+                        PdfWriter.GetInstance(doc, new FileStream(sfd.FileName, FileMode.Create));
+                        doc.Open();
+                        doc.Add(new iTextSharp.text.Paragraph(rtb_data.Text));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        doc.Close();
+                    }
+                }
+            }
+        }
+    }
 }
